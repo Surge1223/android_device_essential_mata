ProtonAOSP/android_device_google_wahoo

Move to config.fs
TARGET_ANDROID_FILESYSTEM_CONFIG_H is soon to be deprecated, so move
to config.fs

Test: build
Change-Id: Ie987da844a8d6747eebc0886d70731eb1d0e4125

BoardConfig.mk
@@ -92,7 +92,7 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/wahoo/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/wahoo/sepolicy/private
 BOARD_SEPOLICY_DIRS += device/google/wahoo/sepolicy/verizon
 
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := device/google/wahoo/android_filesystem_config.h
+TARGET_FS_CONFIG_GEN := device/google/wahoo/config.fs
 
 QCOM_BOARD_PLATFORMS += msm8998
 BOARD_HAVE_BLUETOOTH_QCOM := true

Surge1223/android_device_essential_mata

BoardConfig.mk
@@ -98,7 +98,7 @@ BOARD_SEPOLICY_DIRS += device/essential/mata/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/essential/mata/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/essential/mata/sepolicy/private
 
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := device/essential/mata/android_filesystem_config.h
+TARGET_FS_CONFIG_GEN := device/essential/mata/config.fs
 
 QCOM_BOARD_PLATFORMS += msm8998
 BOARD_HAVE_BLUETOOTH_QCOM := true